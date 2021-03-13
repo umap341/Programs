4a4ce532b0a896fa18046a116667dc556fb8c454
umap341 <80568164+umap341@users.noreply.github.com>
2021-03-12 23:18:52 -0800

    Update My_project.py

--- My_project.py
+++ My_project.py
@@ -1,2 +1,13 @@
-print('Welcome to Google App Engine')
-print('Thank you Prof.Gao for teaching Cloud Technologies')
+from flask import Flask
+
+
+# If `entrypoint` is not defined in app.yaml, App Engine will look for an app
+# called `app` in `main.py`.
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    """Return a friendly HTTP greeting."""
+    return 'print('Welcome to Google App Engine! Thank you Prof Gao for teaching Cloud Technologies')
+ #print('Thank you Prof.Gao for teaching Cloud Technologies')'
